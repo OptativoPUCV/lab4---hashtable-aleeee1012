4d8697f641ef5f3d033e8bd9623ecb8dbdf27e3b
Replit user <>
2023-04-14 20:48:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -5,55 +5,59 @@
 #include <ctype.h>
 #include "hashmap.h"
 
-
 typedef struct HashMap HashMap;
-int enlarge_called=0;
+int enlarge_called = 0;
 
-struct HashMap {
-    Pair ** buckets;
-    long size; //cantidad de datos/pairs en la tabla
-    long capacity; //capacidad de la tabla
-    long current; //indice del ultimo dato accedido
+struct HashMap
+{
+  Pair ** buckets;
+  long size; //cantidad de datos/pairs en la tabla
+  long capacity; //capacidad de la tabla
+  long current; //indice del ultimo dato accedido
 };
 
-Pair * createPair( char * key,  void * value) {
-    Pair * new = (Pair *)malloc(sizeof(Pair));
-    new->key = key;
-    new->value = value;
-    return new;
+Pair * createPair( char * key,  void * value)
+{
+  Pair * new = (Pair *)malloc(sizeof(Pair));
+  new->key = key;
+  new->value = value;
+  return new;
 }
 
-long hash( char * key, long capacity) {
-    unsigned long hash = 0;
-     char * ptr;
-    for (ptr = key; *ptr != '\0'; ptr++) {
-        hash += hash*32 + tolower(*ptr);
-    }
-    return hash%capacity;
+long hash( char * key, long capacity)
+{
+  unsigned long hash = 0;
+  char * ptr;
+  for (ptr = key; *ptr != '\0'; ptr++)
+  {
+    ash += hash*32 + tolower(*ptr);
+  }
+  return hash%capacity;
 }
 
-int is_equal(void* key1, void* key2){
-    if(key1==NULL || key2==NULL) return 0;
-    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
-    return 0;
-}
-
-
-void insertMap(HashMap * map, char * key, void * value) {
-
-
-}
-
-void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
-
+int is_equal(void* key1, void* key2)
+{
+  if(key1==NULL || key2==NULL) return 0;
+  if(strcmp((char*)key1,(char*)key2) == 0) return 1;
+  return 0;
 }
 
 
-HashMap * createMap(long capacity) {
+void insertMap(HashMap * map, char * key, void * value)
+{
 
-    return NULL;
+}
+
+void enlarge(HashMap * map)
+{
+  enlarge_called = 1; //no borrar (testing purposes)
+}
+
+
+HashMap * createMap(long capacity)
+{
+
+  return NULL;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
